5c99d5fb476b931d62e8fd3d6456ca356226ecd6
JasonThon <markmakemate@gmail.com>
2024-02-19 15:41:18 +0800

    [feat/MPS-260] implement rpcx tests

---
+++ build.rs
@@ -0,0 +1,19 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let mut config = prost_build::Config::new();
+    // config
+    //     .out_dir("tests/proto")
+    //     .service_generator(
+    //         tonic_build::configure()
+    //             .build_server(true)
+    //             .build_client(true)
+    //             .service_generator(),
+    //     )
+    //     .compile_protos(
+    //         &[
+    //             "tests/proto/echo/echo.proto",
+    //         ],
+    //         &["tests/proto"],
+    //     )?;
+
+    Ok(())
+}
