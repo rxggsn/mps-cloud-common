89e17b164121ed07ce810dbe5442bb523fbabe69
JasonThon <markmakemate@gmail.com>
2024-06-25 15:08:18 +0800

    [feat/MPS-386] implement get today end/start timestamp

--- src/times.rs
+++ src/times.rs
@@ -9,6 +9,22 @@ pub fn now_timestamp_secs() -> i64 {
     chrono::Local::now().timestamp()
 }
 
+pub fn today_start_timestamp_secs() -> i64 {
+    let now = chrono::Local::now();
+    now.date_naive()
+        .and_hms_milli_opt(0, 0, 0, 0)
+        .map(|datetime| datetime.timestamp())
+        .unwrap_or_default()
+}
+
+pub fn today_end_timestamp_secs() -> i64 {
+    let now = chrono::Local::now();
+    now.date_naive()
+        .and_hms_milli_opt(23, 59, 59, 999)
+        .map(|datetime| datetime.timestamp())
+        .unwrap_or_default()
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -25,5 +41,21 @@ mod tests {
         let now = now_timestamp_secs();
         println!("now: {}", now);
         assert!(now > 0);
+    }
+
+    #[test]
+    fn test_today_start_timestamp_secs() {
+        let now = now_timestamp_secs();
+        let today_start = crate::times::today_start_timestamp_secs();
+        println!("now: {}, today_start: {}", now, today_start);
+        assert!(today_start > 0);
+    }
+
+    #[test]
+    fn test_today_end_timestamp_secs() {
+        let now = now_timestamp_secs();
+        let today_end = crate::times::today_end_timestamp_secs();
+        println!("now: {}, today_end: {}", now, today_end);
+        assert!(today_end > 0);
     }
 }
