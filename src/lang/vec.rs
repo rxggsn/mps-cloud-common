0bdf5034ebf91203171198de4987f8b17ce07f51
jason.thon <songyi@ggsn.com>
2025-03-11 12:24:25 +0800

    add group operation for vector

--- src/lang/vec.rs
+++ src/lang/vec.rs
@@ -1,7 +1,21 @@
+use std::collections::HashMap;
+
 pub fn index_for_each<T, F: FnMut(usize, &T)>(iter: &Vec<T>, mut process: F) {
     let mut idx = 0;
     iter.iter().for_each(|elem| {
         process(idx, elem);
         idx += 1;
     })
+}
+
+pub fn group<T, F: Fn(&T) -> K, K: Eq + std::hash::Hash>(
+    iter: &Vec<T>,
+    key_fn: F,
+) -> HashMap<K, Vec<&T>> {
+    let mut map = HashMap::new();
+    iter.iter().for_each(|elem| {
+        let key = key_fn(elem);
+        map.entry(key).or_insert_with(Vec::new).push(elem);
+    });
+    map
 }
