3c8909265cb2c80768fc197eda8e47e679bd7836
jason.thon <songyi@ggsn.com>
2025-06-01 16:28:32 +0800

    support json utils

--- src/utils.rs
+++ src/utils.rs
@@ -373,6 +373,28 @@ pub fn replace_by_variables(origin: &str, variables: &HashMap<String, String>) -
     new_string
 }
 
+pub mod json {
+    pub fn merge(a: serde_json::Value, b: serde_json::Value) -> serde_json::Value {
+        match (a, b) {
+            (serde_json::Value::Object(mut a_map), serde_json::Value::Object(b_map)) => {
+                b_map.into_iter().for_each(|(key, value)| {
+                    a_map.insert(key, value);
+                });
+                serde_json::Value::Object(a_map)
+            }
+            (a, _) => a,
+        }
+    }
+
+    pub fn merge_inplace(a: &mut serde_json::Value, b: serde_json::Value) {
+        if let (serde_json::Value::Object(a_map), serde_json::Value::Object(b_map)) = (a, b) {
+            b_map.into_iter().for_each(|(key, value)| {
+                a_map.insert(key, value);
+            });
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{env, thread};
